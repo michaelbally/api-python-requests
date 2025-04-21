ef7b4ea010f85b7742fa933b112d39f4034efe5b
Michael Ball <michaelballqa@gmail.com>
2025-04-21 15:43:45 +0100

    first tests

---
+++ tests/conftest.py
@@ -0,0 +1,42 @@
+import pytest
+
+base_url = 'https://restful-booker.herokuapp.com/'
+
+@pytest.fixture(scope="session")
+def baseURL():
+    return base_url
+
+@pytest.fixture(scope="session")
+def creds():
+    return {
+        "username": "admin",
+        "password": "password123"
+    }
+
+@pytest.fixture
+def booking_payload():
+    return {
+        "firstname": "Michael",
+        "lastname": "Ball",
+        "totalprice": 111,
+        "depositpaid": True,
+        "bookingdates": {
+            "checkin": "2018-01-01",
+            "checkout": "2019-01-01"
+        },
+        "additionalneeds": "Breakfast"
+    }
+
+@pytest.fixture
+def updated_payload():
+    return {
+        "firstname": "Michael",
+        "lastname": "Ball",
+        "totalprice": 111,
+        "depositpaid": True,
+        "bookingdates": {
+            "checkin": "2019-01-01",
+            "checkout": "2020-01-01"
+        },
+        "additionalneeds": "no Breakfast"
+    }
---
+++ tests/test_restful_booker.py
@@ -0,0 +1,37 @@
+import requests
+
+bokingID = None
+
+# auth - no creds
+def test_bad_creds(baseURL):
+    response = requests.post(baseURL + 'auth')
+    assert response.status_code == 200
+    assert response.text == '{"reason":"Bad credentials"}'
+
+#auth - valid creds
+def test_valid_creds(baseURL, creds):
+    response = requests.post(baseURL + 'auth', json=creds)
+    assert response.status_code == 200
+    assert 'token' in response.text
+    
+# create booking
+def test_post_booking(baseURL, booking_payload):
+    global bookingId
+    response = requests.post(baseURL + 'booking', json=booking_payload)
+    assert 'bookingid' in response.text
+    assert response.status_code == 200
+    bookingId = response.json()['bookingid']
+
+# update booking
+def test_update_booking(baseURL, updated_payload):
+    global bookingId
+    headers = {'Content-Type': 'application/json', 'Authorization' : 'Basic YWRtaW46cGFzc3dvcmQxMjM='}
+    response = requests.put(baseURL + 'booking/' + f'{bookingId}' , json=updated_payload, headers=headers)
+    assert response.status_code == 200
+
+def test_get_booking(baseURL):
+    global bookingId
+    response = requests.get(baseURL + 'booking/' + f'{bookingId}')
+    data = response.json()
+    assert data['additionalneeds'] == "no Breakfast"
+    assert data['bookingdates']['checkin'] == "2019-01-01"
